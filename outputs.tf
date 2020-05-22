005d3f4f767ce567b3772ce52fd9c1956b30cf49
Sage Rimal <srimal@campusexplorer.com>
2020-05-22 09:24:30 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,13 @@
 
+# Define the output variable for the lambda function.
+
+
+output "role" {
+  description = "The Amazon Resource Name (ARN) identifying your Lambda Function."
+  value       =  "${aws_lambda_function.test_lambda.role}"
+}
+
+output "function_name" {
+  description = "The name of the role for the Lambda Function."
+  value       =  "${aws_lambda_function.test_lambda.function_name}"
+}
