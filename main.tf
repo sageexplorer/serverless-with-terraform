99269c718d4d8e444629d4e625323ce2a04aad0a
Sage Rimal <srimal@campusexplorer.com>
2020-05-22 17:46:11 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,6 +35,37 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = "${aws_iam_role.iam_for_lambda.name}"
+  policy_arn = "${aws_iam_policy.lambda_logging.arn}"
+}
+
+
 resource "aws_lambda_function" "test_lambda" {
   filename      = "lambda_function.zip"
   function_name = "greet_lambda"
@@ -44,6 +75,7 @@ resource "aws_lambda_function" "test_lambda" {
   source_code_hash = "${filebase64sha256("lambda_function.zip")}"
 
   runtime = "python3.7"
+  depends_on = ["aws_iam_role_policy_attachment.lambda_logs", "aws_cloudwatch_log_group.example"]
 
   environment {
     variables = {
@@ -51,3 +83,9 @@ resource "aws_lambda_function" "test_lambda" {
     }
   }
 }
+resource "aws_cloudwatch_log_group" "example" {
+  name              = "greet_lambda"
+  retention_in_days = 14
+}
+
+
